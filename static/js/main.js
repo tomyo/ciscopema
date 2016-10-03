aec0506721fc6bd2a29f8313b536828bcfe8057b
tomyo <tomas@ha-team.co>
2016-10-03 20:11:44 -0300

    First commit of new site

---
+++ static/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $(".video-link").jqueryVideoLightning({
+      id: "x8ErOxGPiGY",
+      autoplay: true,
+      color: "white"
+  });
+  $('.modalDialog').click(function(event){
+    if (event.target.className == 'modalDialog') {
+      event.target.firstElementChild.firstElementChild.click();
+    }
+  });
+  $('#tourDates').click(function(event){
+    $('#tourDatesSection').toggle();
+  });
+});
