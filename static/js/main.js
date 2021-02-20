33f7243edb841d4138fffd94817b78052c629fb5
tomyo <tomyhayes@gmail.com>
2021-02-20 16:10:37 +0100

    Disable video plugin

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-  $(".video-link").jqueryVideoLightning({
-      id: "x8ErOxGPiGY",
-      autoplay: true,
-      color: "white"
-  });
+  // $(".video-link").jqueryVideoLightning({
+  //     id: "x8ErOxGPiGY",
+  //     autoplay: true,
+  //     color: "white"
+  // });
   $('.modalDialog').click(function(event){
     if (event.target.className == 'modalDialog') {
       event.target.firstElementChild.firstElementChild.click();
