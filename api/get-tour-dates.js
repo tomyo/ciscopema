dd4c91d5d34aecd8c2642a3db9bd24968a33f041
Tomás Hayes <tomas@hayes.com.ar>
2022-09-09 16:11:20 +0200

    Test vercel edge function

--- api/get-tour-dates.js
+++ api/get-tour-dates.js
@@ -1,10 +1,15 @@
-export default async function handler(request, response) {
-  const htmlText = await (await fetch('https://www.songkick.com/artists/9553324-cisco-pema')).text();
+export const config = {
+  runtime: 'experimental-edge',
+};
+
+export default async (req) => {
+  const songkickArtistUrl = 'https://www.songkick.com/artists/9553324-cisco-pema';
+  const htmlText = await (await fetch(songkickArtistUrl)).text();
   const regExp = /(<ol.+artist-calendar-summary[\w\W]+?)<p class="see-all"/i;
 
   if (!htmlText.match(regExp)) {
-    return response.status(404).end();
+    return new Response().status(404).end();
   }
 
-  response.end(htmlText.match(regExp)[1]);
+  return new Response(htmlText.match(regExp)[1]);
 }
