abe1fdd9108e85e06e59ad1d61288dcf26aea6fa
Tomás Hayes <tomas@hayes.com.ar>
2022-09-14 00:03:19 +0200

    Tidy up code a bit

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-const pressInfoButton = document.querySelector('#press-info');
-const dialog = document.querySelector('dialog');
 
-pressInfoButton.addEventListener('click', () => dialog.showModal());
+function setUpPressInfoModal() {
+  const pressInfoButton = document.querySelector('#press-info');
+  const dialog = document.querySelector('dialog');
 
-
-// i18n
-const { useL10n } = await import('./lib/i18n/use-l10n.js');
-const [getUILanguage, getPreferredLanguage, translateInto] = useL10n({ filesPath: 'l10n/' });
-
-const langSwitcher = document.querySelector("#lang-switcher");
-const availableLanguages = [...langSwitcher.options].map(o => o.value);
-
-if (availableLanguages.includes(getPreferredLanguage())) {
-  await translateInto(getPreferredLanguage());
+  pressInfoButton.addEventListener('click', () => dialog.showModal());
 }
 
-langSwitcher.value = getUILanguage();
+async function localize() {
+  const { useL10n } = await import('./lib/i18n/use-l10n.js');
+  const [getUILanguage, getPreferredLanguage, translateInto] = useL10n({ filesPath: './l10n/' });
+  const langSwitcher = document.querySelector("#lang-switcher");
 
-langSwitcher.addEventListener('change', (e) => translateInto(e.target.value));
+  langSwitcher.addEventListener('change', (e) => translateInto(e.target.value));
+
+  if (getUILanguage() !== getPreferredLanguage()) {
+    await translateInto(getPreferredLanguage());
+  }
+
+  langSwitcher.value = getUILanguage();
+}
+
+
+setUpPressInfoModal();
+localize();
