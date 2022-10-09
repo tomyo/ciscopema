3042bbb2a5a9c26b51bfb51416585dacb779f11b
Tomás Hayes <tomas@hayes.com.ar>
2022-10-09 13:49:24 +0200

    Make api/get-tour-dates regExp more rebust

--- api/get-tour-dates.js
+++ api/get-tour-dates.js
@@ -5,7 +5,7 @@ export const config = {
 export default async (req) => {
   const songkickArtistUrl = 'https://www.songkick.com/artists/9553324-cisco-pema';
   const htmlText = await (await fetch(songkickArtistUrl)).text();
-  const regExp = /(<ol.+artist-calendar-summary[\w\W]+?)<p class="see-all"/i;
+  const regExp = /(<ol.+artist-calendar-summary[\w\W]+?)\s*<p class=".*see-all/i;
 
   if (!htmlText.match(regExp)) {
     return new Response().status(404).end();
