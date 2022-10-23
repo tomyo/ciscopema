102531349f511c61ec5215616564dea8698a9e77
Tomás Hayes <tomas@hayes.com.ar>
2022-10-23 16:42:38 +0200

    Refactor tour-dates element to use loading="lazy" attribute

--- components/tour-dates.js
+++ components/tour-dates.js
@@ -2,40 +2,40 @@ const tourDatesApiUrl = '/api/get-tour-dates/';
 const tourLinksUrlPrefix = 'https://www.songkick.com/';
 
 async function fetchEventListHtml() {
-  function adaptUrls(text) {
+  function prefixUrls(text) {
     return text.replaceAll(/(href=")\//gi, `$1${tourLinksUrlPrefix}`);
   }
 
   const response = await fetch(tourDatesApiUrl);
   if (!response?.ok) throw new Error(`Error getting tour-dates, reponse status: ${response.status}`);
 
-  return adaptUrls(await response.text());
+  return prefixUrls(await response.text());
 }
 
 customElements.define('tour-dates', class extends HTMLElement {
   constructor() {
     super();
-    this.setUpFetchWhenVisible();
+    this.setUpEventsFetching();
   }
 
-  setUpFetchWhenVisible() {
-    /* Only fetch and process events when <tour-dates> is visible */
-    let callback = (entries, observer) => {
-      if (![...entries][0].isIntersecting) return;
+  setUpEventsFetching() {
+    if (this.getAttribute('loading') == 'lazy') {
+      /* Only fetch and format events when component is visible */
+      new IntersectionObserver((entries, observer) => {
+        if (![...entries][0].isIntersecting) return;
 
+        this.fetchAndRenderEventList();
+        observer.unobserve(this);
+      }).observe(this);
+    } else {
       this.fetchAndRenderEventList();
-      observer.unobserve(this);
-    };
-
-    new IntersectionObserver(callback).observe(this);
+    }
   }
 
   async fetchAndRenderEventList() {
     try {
       const eventListNode = await fetchAndFormatEventList(this.getLocale());
-      this.attachShadow({ mode: 'open' });
-      this.shadowRoot.appendChild(eventListNode);
-      this.appendStyles();
+      this.attachShadow({ mode: 'open' }).appendChild(eventListNode);
     } catch (error) {
       console.error(error.message);
       console.info('Using original widget as fallback ...');
@@ -47,9 +47,61 @@ customElements.define('tour-dates', class extends HTMLElement {
     return document.documentElement.lang || this.getAttribute('lang') || 'en';
   }
 
-  appendStyles() {
-    const style = document.createElement('style');
-    style.innerHTML = /*css*/`
+  insertFallbackContent() {
+    // Use original songkick widget
+    this.innerHTML = `
+      <a href="https://www.songkick.com/artists/9553324" class="songkick-widget" 
+        data-font-color="black" data-track-button="off" data-theme="light"
+        data-locale="en" data-background-color="transparent" data-detect-style="true">
+        Cisco Pema
+      </a>
+    `
+    const script = document.createElement('script');
+    script.setAttribute('src', "//widget.songkick.com/9553324/widget.js");
+    this.appendChild(script);
+  }
+});
+
+/**
+ * 
+ * @param {String} locale 
+ * @returns A DOM node containing the fetched and formated even list content
+ */
+async function fetchAndFormatEventList(locale = 'en') {
+  const template = document.createElement('template');
+  template.innerHTML = await fetchEventListHtml();
+
+  function formatDate(date) {
+    const options = { weekday: "short", month: "long", day: "numeric" };
+    return new Date(date).toLocaleDateString(locale, options);
+  }
+
+  // adjustDateFormat
+  for (const dateBox of template.content.querySelectorAll('.date-box')) {
+    const date = dateBox.previousElementSibling.dateTime;
+    dateBox.innerText = formatDate(date).replace(',', '');
+  }
+
+  // adjustEventsLocation
+  for (const div of template.content.querySelectorAll('.event-details')) {
+    const strong = div.querySelector('.primary-detail');
+    const p = document.createElement('p');
+    p.innerText = strong.innerText;
+    strong.remove();
+    div.appendChild(p);
+  }
+
+  // addTicketButtons
+  for (const a of template.content.querySelectorAll('.event-listing a')) {
+    const button = document.createElement('button');
+    button.innerText = 'Tickets';
+    button.part = 'button';
+    a.appendChild(button);
+  }
+
+  // Add style
+  const style = document.createElement('style');
+  style.innerHTML = /*css*/`
       :host {
         display: block;
         line-height: 1.231em;
@@ -101,55 +153,7 @@ customElements.define('tour-dates', class extends HTMLElement {
         line-height: 1.4em;
       }
     `
-    this.shadowRoot.appendChild(style);
-  }
-
-  insertFallbackContent() {
-    // Use original songkick widget
-    this.innerHTML = `
-      <a href="https://www.songkick.com/artists/9553324" class="songkick-widget" 
-        data-font-color="black" data-track-button="off" data-theme="light"
-        data-locale="en" data-background-color="transparent" data-detect-style="true">
-        Cisco Pema
-      </a>
-    `
-    const script = document.createElement('script');
-    script.setAttribute('src', "//widget.songkick.com/9553324/widget.js");
-    this.appendChild(script);
-  }
-});
-
-async function fetchAndFormatEventList(locale = 'en') {
-  const template = document.createElement('template');
-  template.innerHTML = await fetchEventListHtml();
-
-  function formatDate(date) {
-    const options = { weekday: "short", month: "long", day: "numeric" };
-    return new Date(date).toLocaleDateString(locale, options);
-  }
-
-  // adjustDateFormat
-  for (const dateBox of template.content.querySelectorAll('.date-box')) {
-    const date = dateBox.previousElementSibling.dateTime;
-    dateBox.innerText = formatDate(date).replace(',', '');
-  }
-
-  // adjustEventsLocation
-  for (const div of template.content.querySelectorAll('.event-details')) {
-    const strong = div.querySelector('.primary-detail');
-    const p = document.createElement('p');
-    p.innerText = strong.innerText;
-    strong.remove();
-    div.appendChild(p);
-  }
-
-  // addTicketButtons
-  for (const a of template.content.querySelectorAll('.event-listing a')) {
-    const button = document.createElement('button');
-    button.innerText = 'Tickets';
-    button.part = 'button';
-    a.appendChild(button);
-  }
+  template.content.appendChild(style);
 
   return template.content;
 }
