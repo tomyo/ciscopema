761f8e478eb62c51eeb46c96e14e7dd60d98e36e
tomyo <tomyhayes@gmail.com>
2024-03-25 17:32:20 -0300

    Prettify index.js

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-
 function setUpPressInfoModal() {
-  const pressInfoButton = document.querySelector('#press-info');
-  const dialog = document.querySelector('dialog');
+  const pressInfoButton = document.querySelector("#press-info");
+  const dialog = document.querySelector("dialog");
 
-  pressInfoButton.addEventListener('click', () => dialog.showModal());
+  pressInfoButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    dialog.showModal();
+  });
 }
 
 async function localize() {
-  const { useL10n } = await import('./lib/i18n/use-l10n.js');
-  const [getUILanguage, getPreferredLanguage, translateInto] = useL10n({ filesPath: './l10n/' });
+  const { useL10n } = await import("./lib/i18n/use-l10n.js");
+  const [getUILanguage, getPreferredLanguage, translateInto] = useL10n({
+    filesPath: "./l10n/",
+  });
   const langSwitcher = document.querySelector("#lang-switcher");
 
-  langSwitcher.addEventListener('change', (e) => translateInto(e.target.value));
+  langSwitcher.addEventListener("change", (e) => translateInto(e.target.value));
 
   if (getUILanguage() !== getPreferredLanguage()) {
     await translateInto(getPreferredLanguage());
@@ -19,7 +23,6 @@ async function localize() {
 
   langSwitcher.value = getUILanguage();
 }
-
 
 setUpPressInfoModal();
 localize();
